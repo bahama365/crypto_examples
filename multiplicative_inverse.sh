3bf678acf0f2c8a04bb181a70a3b541288cf3a0f
bahama365 <31001392+bahama365@users.noreply.github.com>
2019-03-19 13:39:58 +0000

    Add files via upload

--- multiplicative_inverse.sh
+++ multiplicative_inverse.sh
@@ -8,6 +8,8 @@ read -p "What is your value for 'n'": n
 read -p "What is your value for 'm'": m
 temp1=$(mktemp -t temp1.XXXXXX)
 val=1
+echo "Calculating '(x * n) mod m' where x is an integer starting at 1 and increasing. All answers must be unique for x to be the multiplicative inverse.."
+echo "Working...."
 while true
 do
   ans=$(echo "($val * $n) % $m" | bc)
